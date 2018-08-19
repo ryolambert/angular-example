d7386bbc2ea915e7ea36b4b854bc4cac1966559f
James R. Lambert <ryolambert@gmail.com>
2018-08-18 19:12:44 -0600

    fixed bootstrap import, package config for most up to date modules, and changed table format for bootstrap 4 syntax.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import {HttpClientModule} from "@angular/common/http";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const moduleDeclarations = [AppComponent];
 
 @NgModule({
-	imports:      [BrowserModule, HttpClientModule, routing],
+	imports:      [ NgbModule.forRoot(), BrowserModule, HttpClientModule, routing],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
 	providers:    [...appRoutingProviders]
--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {User} from "../shared/interfaces/user";
 import {UserService} from "../shared/services/user.service";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
 	template: require("./splash.component.html")
